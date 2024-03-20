6d84513fb69c417739159a17a338e29218125c4e
Oscar Beller <gabrieloscarbeller@hotmail.com>
2024-03-19 22:05:19 -0300

    Segundo desafio

--- productManager.js
+++ productManager.js
@@ -1,9 +1,41 @@
+const fs = require( 'fs' ); 
+
 class Productmanager {
 
     #products;
+    #path;
     static idProducto = 0;
+
     constructor (){
+        this.#path = 'data/productos.json';
+        this.#products = this.#leerProductoInFile();
+    }
+
+    #asignarIdProducto(){
+        let id = 1;
+        if (this.#products.length != 0) 
+            id = this.#products [this.#products.length - 1].id + 1 ;
+        return id;
+    }
+
+
+    #leerProductoInFile(){
         this.#products = [];
+        try{
+            if(fs.existsSync(this.#path))
+                return json.parse(fs.readFileSync(this.#path,  'utf8'));
+          return [];
+        }catch (error) {
+            console.log(`Ocurrio un erro al intentar leer el archivo : ${error}`);
+        }
+    }
+
+    #almacenarArchivo(){
+        try {
+            fs.writeFileSync(this.#path, JSON.stringify(this.#products));
+        } catch (error) {
+            console.log(`Ocurrio un erro al intentar de almacenar el archivo : ${error}`);
+        }
     }
 
     addProduct(title, description, price, thumbnail, code, stock) {
@@ -11,12 +43,12 @@ class Productmanager {
         return 'Fijese de haber ingresado correctamente los siguientes datos (title, description, price, thumbnail, code, stock)'
 
 
-     const codeRep = this.#products.some(p => p.code == code)
-     if (codeRep)
+        const codeRepetido = this.#products.some(p => p.code == code)
+     if (codeRepetido)
         return `El id ${code} ya esta ocupado, ingrese otro por favor`
         
         Productmanager.idProducto = Productmanager.idProducto +1;
-        const id = Productmanager.idProducto
+        const id = this.#asignarIdProducto();
         const newProduct = {
             id,
             title,
@@ -27,21 +59,47 @@ class Productmanager {
             stock
         };
         this.#products.push(newProduct)
+        this.#almacenarArchivo()
         
-        return'Producto añaido con exito'
+        return `Producto añadido con exito`
     }
     getProducts(){
         return this.#products
     }
-    getProductsById(id){
+    getProductsById(id,){
         const product = this.#products.find(p => p.id == id)
         if (product)
             return product
         else
             return `Not Found el id ${id}`
     }
+
+    updateProduct(id, propProduc){
+        let msg = `El prodcuto con el id ${id} no existe`;
+        
+        const index = this.#products.findIndex(p=>p.id===id);
+
+        if  (index !== -1){
+            const {id, ...rest} = propProduc;
+            this.#products[index]={...this.#products[index], ...rest};
+            this.#almacenarArchivo();
+            msg = 'Producto Actualizado';
+        } return msg;
+    }
+
+    deleteProdct(id){
+        let msid = `El producto con el id ${id} no fue encontrado`;
+        
+        const index = this.#products.findIndex(p=> p.id ===id);
+        if(index !== -1){
+            this.#products = this.#products.filter(p=> p.id !== id);    
+            this.#almacenarArchivo();
+            msid = `Producto eliminado corectamente`;      
+        }
+        return msid;
+    }
 }
 
 
 
-module.exports = Productmanager
+module.exports = Productmanager;
--- testing.js
+++ testing.js
@@ -5,4 +5,4 @@ const  producto = new ProductManager();
 console.log(producto.addProduct('Camionero','mate',12500,'https://mate1.com','mateCamionero',15))
 console.log(producto.addProduct('Imperial','mate',13500,'https://portatamate.com','mateImperial',5))
 
-console.log(producto.getProducts())
+// console.log(producto.getProducts())
